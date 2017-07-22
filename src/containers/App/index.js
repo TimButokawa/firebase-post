5d865582aaac644bd6f4dbae01b73a9b08e590fe
Tim Butorac <tim@timbutorac.com>
2017-07-22 07:05:55 -0700

    add react router and firebase to app - WIP add data ref to app

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,6 +1,24 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
+import config from '../../utils/firebase-config';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    firebase.initializeApp(config);
+  }
+
+  componentWillMount() {
+    const posts = firebase.database().ref('posts');
+
+    posts.on('value', posts => {
+      this.setState({
+        posts: posts.val(),
+        loading: false
+      });
+    });
+  }
+
   render() {
     return (
       <div>Hello World</div>
---
+++ src/utils/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyA1Niq3Mk4O4sWwJsGr-7vJ-7CkxQnP5kg",
+  authDomain: "fir-reddit.firebaseapp.com",
+  databaseURL: "https://fir-reddit.firebaseio.com",
+  projectId: "fir-reddit",
+  storageBucket: "fir-reddit.appspot.com",
+  messagingSenderId: "411949907853"
+};
