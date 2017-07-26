4081237c596b5081a4346f28fec3d8db66ccd1ff
Tim Butorac <tim@timbutorac.com>
2017-07-26 08:18:30 -0700

    wip begin create posts container

---
+++ src/containers/CreatePost/index.js
@@ -0,0 +1,65 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {browserHistory} from 'react-router';
+
+class CreatePost extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      title: ''
+    };
+  }
+
+  handleChange(e) {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const {postsRef} = this.props;
+
+    if (this.state.title.length) {
+      postsRef.push({
+        title: this.state.title
+      });
+
+      this.setState({
+        title: ''
+      });
+
+      browserHistory.push('posts');
+    }
+  }
+
+  render() {
+    return(
+      <form
+        name="createPostForm"
+        onSubmit={this.handleSubmit}>
+        <div>
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={this.handleChange}
+            value={this.state.title}
+            required/>
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    );
+  }
+};
+
+CreatePost.propTypes = {
+  postsRef: PropTypes.object
+};
+
+export default CreatePost;
