9574e9d6a4c5310cba1aa27cdcb0248df1cc5371
Tim Butorac <tim@timbutorac.com>
2017-07-26 09:51:40 -0700

    add firebase ref to props, create vote handler and update firebase ref

--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -2,6 +2,17 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Posts extends Component {
+  constructor(props) {
+    super(props);
+    this.handleVote = this.handleVote.bind(this);
+  }
+
+  handleVote(post, key, voteType) {
+    const {firebaseRef} = this.props;
+    post.points = voteType === 'upVote' ? post.points + 1 : post.points - 1;
+    firebaseRef.ref('posts/' + key).set({...post});
+  }
+
   render () {
     const {posts, loading} = this.props;
     const content = loading ? (
@@ -9,7 +20,14 @@ class Posts extends Component {
     ) : (
       <div>
         {Object.keys(posts).map(i => {
-          return <div key={i}>{posts[i].title}</div>;
+          return (
+            <div key={i}>
+              <span>{posts[i].points}</span>
+              <span>{posts[i].title}</span>
+              <button onClick={() => this.handleVote(posts[i], i, 'upVote')}>upvote</button>
+              <button onClick={() => this.handleVote(posts[i], i, 'downVote')}>downvote</button>
+            </div>
+          );
         })}
       </div>
     );
@@ -23,7 +41,8 @@ class Posts extends Component {
 
 Posts.propTypes = {
   posts: PropTypes.object,
-  loading: PropTypes.bool
+  loading: PropTypes.bool,
+  firebaseRef: PropTypes.object
 };
 
 export default Posts;
