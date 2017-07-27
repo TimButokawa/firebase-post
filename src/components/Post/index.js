58a8b02029abc229fc92962d1a459f5eeb77f64d
Tim Butorac <tim@timbutorac.com>
2017-07-27 13:18:59 -0700

    add material card to post component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,17 +1,55 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {Card, CardHeader, CardText} from 'material-ui/Card';
+
+const styles = {
+  container: {
+    margin: '0 0 10px 0'
+  },
+  well: {
+    padding: '5px',
+    backgroundColor: '#eeeeee',
+    borderRadius: '2px'
+  },
+  vote: {
+    fontSize: '1em',
+    color: '#e3e3e3',
+    cursor: 'pointer',
+    margin: '2px',
+    userSelect: 'none',
+    MozUserSelect: 'none',
+    WebkitUserSelect: 'none'
+  }
+};
 
 class Post extends Component {
   render() {
     const {post, id, handleVote} = this.props;
+    const vote = (
+      <span>
+        <i
+          style={styles.vote}
+          className="material-icons"
+          onClick={() => handleVote(post, id, 'upVote')}>thumb_up</i>
+        <i
+          style={styles.vote}
+          className="material-icons"
+          onClick={() => handleVote(post, id, 'downVote')}>thumb_down</i>
+      </span>
+    );
     return (
-      <div>
-        <span>{post.points}</span>
-        <button onClick={() => handleVote(post, id, 'upVote')}>upvote</button>
-        <button onClick={() => handleVote(post, id, 'downVote')}>downvote</button>
-        <span>{post.title}</span>
-        <div>{post.body}</div>
-      </div>
+      <Card style={styles.container}>
+        <CardHeader
+          title={post.title}
+          subtitle={post.points}
+          avatar={vote}
+          showExpandableButton/>
+        <CardText expandable>
+          <div style={styles.well}>
+            {post.body}
+          </div>
+        </CardText>
+      </Card>
     );
   }
 }
