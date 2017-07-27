c0d0272266d1b8ff95eb4cec694d27854a3cef5a
Tim Butorac <tim@timbutorac.com>
2017-07-27 13:18:39 -0700

    add material ui to app

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import {Router, Route} from 'react-router';
+import {Router, Route, IndexRedirect} from 'react-router';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import App from './containers/App';
 import Posts from './containers/Posts';
 import CreatePost from './containers/CreatePost';
 
+injectTapEventPlugin();
+
 const Routes = props => (
-  <Router {...props}>
-    <Route path="/" component={App}>
-      <Route path="posts" component={Posts}/>
-      <Route path="add-post" component={CreatePost}/>
-    </Route>
-  </Router>
+  <MuiThemeProvider>
+    <Router {...props}>
+      <Route path="/" component={App}>
+        <IndexRedirect to="posts" />
+        <Route path="posts" component={Posts}/>
+        <Route path="add-post" component={CreatePost}/>
+      </Route>
+    </Router>
+  </MuiThemeProvider>
 );
 
 export default Routes;
