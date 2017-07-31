39bee326c818a27cec3237c701c307af402f133f
Tim Butorac <tim@timbutorac.com>
2017-07-31 10:28:17 -0700

    add style to post form - update link to browser history push for navigation

--- src/containers/CreatePost/index.js
+++ src/containers/CreatePost/index.js
@@ -1,6 +1,22 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import {browserHistory, Link} from 'react-router';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  container: {
+    padding: '16px'
+  },
+  formGroup: {
+    marginBottom: '10px',
+    maxWidth: '600px'
+  },
+  roomRight: {
+    marginRight: '10px'
+  }
+};
 
 class CreatePost extends Component {
   constructor(props) {
@@ -8,6 +24,7 @@ class CreatePost extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleCancel = this.handleCancel.bind(this);
 
     this.state = {
       title: '',
@@ -41,30 +58,49 @@ class CreatePost extends Component {
     }
   }
 
+  handleCancel() {
+    browserHistory.push('posts');
+  }
+
   render() {
     return(
-      <form
-        name="createPostForm"
-        onSubmit={this.handleSubmit}>
-        <div>
-          <input
-            type="text"
-            name="title"
-            placeholder="Title"
-            onChange={this.handleChange}
-            value={this.state.title}
-            required/>
-          <textarea
-            placeholder="What is it about"
-            name="body"
-            onChange={this.handleChange}
-            value={this.state.body}></textarea>
-        </div>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-        <Link to="/posts">View Posts</Link>
-      </form>
+      <Paper style={styles.container}>
+        <form
+          name="createPostForm"
+          onSubmit={this.handleSubmit}>
+          <div style={styles.formGroup}>
+            <TextField
+              type="text"
+              name="title"
+              floatingLabelText="Title"
+              onChange={this.handleChange}
+              value={this.state.title}
+              required/>
+          </div>
+          <div style={styles.formGroup}>
+            <TextField
+              floatingLabelText="Description"
+              name="body"
+              onChange={this.handleChange}
+              value={this.state.body}
+              multiLine
+              fullWidth
+              required/>
+          </div>
+          <div style={styles.formGroup}>
+            <RaisedButton
+              style={styles.roomRight}
+              label="Cancel"
+              type="button"
+              onClick={this.handleCancel}
+              secondary/>
+            <RaisedButton
+              label="Submit"
+              primary
+              type="submit"/>
+          </div>
+        </form>
+      </Paper>
     );
   }
 };
